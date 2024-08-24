cc0290df7716d5b4ed83ffc0aff6935caa52ea44
Italo Lima Ferreira <ferreiraitalo180@gmail.com>
2024-08-24 15:21:52 -0300

    programei para as anim funcionar e camera seguir player

--- CromaticVoyage/Assets/CromaticVoyage/Scripts/PlayerControllerV2.cs
+++ CromaticVoyage/Assets/CromaticVoyage/Scripts/PlayerControllerV2.cs
@@ -11,6 +11,9 @@ public class PlayerControllerV2 : MonoBehaviour
     private Rigidbody2D rig;
     private bool isJumping;
 
+    [SerializeField] private Animator animator;
+
+
     private Stack<Command> _playerCommands;
     private Vector2 _moveDirection;
     private BoxCollider2D playerCollider2D;
@@ -25,21 +28,37 @@ public class PlayerControllerV2 : MonoBehaviour
 
     private void Update()
     {
+        animator.SetFloat("Speed", Mathf.Abs(_moveDirection.x));
         Jump();
         Move();
         Escaneando();
+        Attack();
     }
 
     public void Jump()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !isJumping)
         {
-            if (!isJumping)
-            {
-                rig.AddForce(new Vector2(0, jumpForce * 2), ForceMode2D.Impulse);
-                isJumping = true;
-            }
+            isJumping = true;
+            animator.SetBool("IsJumping", true);
+            rig.AddForce(new Vector2(0, jumpForce * 2), ForceMode2D.Impulse);
+
+            // Inicia uma corrotina para aguardar o final do pulo
+            StartCoroutine(EndJump());
         }
+    }
+
+    private IEnumerator EndJump()
+    {
+        // Aguarda até que a velocidade vertical diminua, indicando que o pulo terminou
+        while (rig.velocity.y > 0)
+        {
+            yield return null; // Espera um frame
+        }
+
+        // Define o estado de pulo como falso e atualiza o Animator
+        isJumping = false;
+        animator.SetBool("IsJumping", false);
     }
 
     public void Move()
@@ -54,6 +73,40 @@ public class PlayerControllerV2 : MonoBehaviour
         {
             transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
+    }
+    
+    public void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.J) || Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.L))
+        {
+            animator.SetTrigger("IsAtk");
+
+            // Aqui você pode adicionar lógica para cada ataque específico
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                Debug.Log("Ataque 1 executado");
+            }
+            else if (Input.GetKeyDown(KeyCode.K))
+            {
+                Debug.Log("Ataque 2 executado");
+            }
+            else if (Input.GetKeyDown(KeyCode.L))
+            {
+                Debug.Log("Ataque 3 executado");
+            }
+
+            // Inicia a corrotina para resetar o parâmetro de ataque
+            StartCoroutine(ResetAttack());
+        }
+    }
+
+    private IEnumerator ResetAttack()
+    {
+        // Aguarda um tempo para que a animação de ataque termine
+        yield return new WaitForSeconds(0.5f); // Ajuste o tempo conforme a duração da sua animação
+
+        // Reseta o parâmetro de ataque
+        animator.ResetTrigger("IsAtk");
     }
 
     private void FixedUpdate()
