798af6b2ebb21c38350a8bdbc154a39a10712aca
Italo Lima Ferreira <ferreiraitalo180@gmail.com>
2024-11-22 01:19:15 -0300

    falta terminar a primeira cutscene

--- CromaticVoyage/Assets/CromaticVoyage/Scripts/LoadFase1.cs
+++ CromaticVoyage/Assets/CromaticVoyage/Scripts/LoadFase1.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadFase1 : MonoBehaviour
 {
+    public string cenaParaCarregar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.LoadScene(cenaParaCarregar);
     }
 
     // Update is called once per frame
